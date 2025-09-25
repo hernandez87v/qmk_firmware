e8c04ecbb5a3b9a8c0f7283d99b5f97193a96853
hernandez87v <hernandez87v@gmail.com>
2025-09-25 06:00:21 -0700

    fix mistype

--- keyboards/sofle/keymaps/vladh/keymap.c
+++ keyboards/sofle/keymaps/vladh/keymap.c
@@ -187,9 +187,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|------+-------+--------+--------+--------+------|                          |--------+-------+--------+--------+--------+---------|
   UG_TOGG,XXXXXXX,EE_CLR,QK_BOOT,XXXXXXX,MS_WHLU,                    KC_P4, KC_P5, KC_P6, KC_PAST, KC_PGUP, KC_BSPC,
   //|------+-------+--------+--------+--------+------|                          |--------+-------+--------+--------+--------+---------|
-  UG_NEXT,UG_HUEU,UG_SATU,UG_VALU,_______,KMS_WHLD,                   KC_P1, KC_P2, KC_P3, KC_PMNS, KC_PGDN, KC_DEL,
+  UG_NEXT,UG_HUEU,UG_SATU,UG_VALU,_______,MS_WHLD,                   KC_P1, KC_P2, KC_P3, KC_PMNS, KC_PGDN, KC_DEL,
   //|------+-------+--------+--------+--------+------|  ===  |          |  ===  |--------+-------+--------+--------+--------+---------|
-  UG_PREV,UG_HUED,UG_SATD,UG_VALD,MS_WHLL,KMS_WHLR,_______,   _______,KC_PENT, KC_P0, KC_PDOT, KC_PPLS, KC_END, _______,
+  UG_PREV,UG_HUED,UG_SATD,UG_VALD,MS_WHLL,MS_WHLR,_______,   _______,KC_PENT, KC_P0, KC_PDOT, KC_PPLS, KC_END, _______,
   //|------+-------+--------+--------+--------+------|  ===  |          |  ===  |--------+-------+--------+--------+--------+---------|
         _______, _______, _______, _______, _______,          _______, KC_CALC, KC_NUM, _______, _______
   //            \--------+--------+--------+---------+-------|          |--------+---------+--------+---------+-------/
